c2042be50e2f029ae28ef7654fe3dde9ceab1475
abhikante <akante@ufl.edu>
2018-10-16 22:13:27 -0400

    Added item controller

--- store/src/main/java/com/store/app/Application.java
+++ store/src/main/java/com/store/app/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
-
 import com.store.dao.*;
 import com.store.model.*;
 
@@ -40,6 +39,24 @@ public class Application implements CommandLineRunner {
 			Then, you can add calls to your CRUD operations from within this method.
 
 		**/
+        /*jdbcTemplate.execute("SHOW TABLES");
+        jdbcTemplate.execute("DROP TABLE IF EXISTS products, customers, orders");
+        jdbcTemplate.execute("SHOW TABLES");
+        jdbcTemplate.execute("CREATE TABLE customers(id SERIAL, fname VARCHAR(255), lname VARCHAR(255), username VARCHAR(255),email VARCHAR(255), UNIQUE(id, username));")
+        jdbcTemplate.execute("CREATE TABLE products(itemId SERIAL, name VARCHAR(255), msrp DECIMAL(6,2), salePrice DECIMAL(8,2), \n" +
+                "\tupc INT, shortDescription VARCHAR(255), brandName VARCHAR(255), \n" +
+                "\tsize VARCHAR(255), color VARCHAR(255), gender VARCHAR(255), UNIQUE(itemId));");
+        jdbcTemplate.execute("SHOW TABLES");
+
+        jdbcTemplate.execute("INSERT INTO customers (fname, lname, username,email) VALUES (\"Dr. P\", \"Brown\", \"prbrown\", \"philippa.brown\")");
+        jdbcTemplate.execute("SELECT * FROM customers");
+        jdbcTemplate.execute("DROP PROCEDURE IF EXISTS insertProducts");
+        jdbcTemplate.execute("DELIMITER // CREATE PROCEDURE insertProducts (IN numProducts INT)");
+        jdbcTemplate.execute("BEGIN");*/
+
+        jdbcTemplate.execute("SOURCE C:\\Users\\abhik\\WebApp2\\store\\src\\scripts\\createDBTables.sql");
+        jdbcTemplate.execute("SOURCE C:\\Users\\abhik\\WebApp2\\store\\src\\scripts\\populateDBTables.sql");
+
 
     }
 }
